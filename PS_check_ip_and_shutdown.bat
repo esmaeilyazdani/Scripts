f0f7b42f83117ff07f9337327b21bfcefd8b6864
Esmaeil Yazdani <145452138+esmaeilyazdani@users.noreply.github.com>
2025-06-02 11:11:17 +0330

    Update and rename check_ip_and_shutdown.bat to PS_check_ip_and_shutdown.bat

---
+++ PS_check_ip_and_shutdown.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "C:\ProgramData\Check_IP\check_ip_change_and_shutdown.ps1"
--- check_ip_and_shutdown.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "C:\ProgramData\Check_IP\check_ip_change_and_shutdown.ps1"
